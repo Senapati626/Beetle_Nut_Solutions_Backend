2b8681e580ed8c6e0646841ad7e4e3d6aad7f25b
Neellohit <neellohitsenapati626@gmail.com>
2021-08-26 02:05:04 +0530

    babi bhalo

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const mysql = require('mysql');
-const io = require('socket.io');
 const cors = require('cors');
 
 
 const app = express();
-app.use(cors());
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+app.use(cors()); // Used to remove cors error.
 app.use(express.json())
+
+//Storing mysql database inside the 'databass' variable;
 const database = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -14,6 +18,7 @@ const database = mysql.createConnection({
     database: "beetle_nut"
 });
 
+//Connecting the database with my server.js
 database.connect((err)=>{
     if(err){
         throw err
@@ -24,6 +29,8 @@ database.connect((err)=>{
 app.get('/',(req,res)=>{
     res.send('BeatleNuts are online.')
 })
+
+//Post request showing all branches from the beetle_nut table which matches the pin code inputted by user.
 app.post('/shows',(req,res)=>{
     const pin = '%'+req.body.pin+'%';
     database.query(`SELECT * FROM beetle_nut WHERE Pincode_covered like ?`,pin,(err,result)=>{
@@ -31,11 +38,12 @@ app.post('/shows',(req,res)=>{
         res.send(result)
     });
 })
+
+// Post request to insert client details inside the alerts.
 app.post('/insert',(req,res)=>{
     const pin = '%'+req.body.pin+'%';
     const name = req.body.name;
     const email = req.body.email;
-    console.log(req.body);
     const users = [];
     database.query(`SELECT username FROM beetle_nut WHERE Pincode_covered like ?`,pin,(err,result)=>{
         if(err) console.log(err);
@@ -54,10 +62,12 @@ app.post('/insert',(req,res)=>{
         })
     })
 })
+
+// Post request for branch sign in.
 app.post('/signin',(req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
-    console.log(req.body)
+    //console.log(req.body)
     if(!username || !password){
         return res.status(400).json('Cannot Log In');
     }
@@ -66,6 +76,8 @@ app.post('/signin',(req,res)=>{
         res.send(result)
     })
 })
+
+// Post request for displaying alerts within a branch.
 app.post('/alerts',(req,res)=>{
     const username = req.body.username;
     database.query(`SELECT * FROM alerts WHERE branch_username=? ORDER BY date DESC`,username,(err,result)=>{
@@ -73,6 +85,8 @@ app.post('/alerts',(req,res)=>{
         res.send(result)
     })
 })
+
+// Post request for counting the number of alerts for a particular branch.
 app.post('/count',(req,res)=>{
     const username = String(req.body.username);
     database.query(`SELECT COUNT(*) AS count FROM alerts WHERE branch_username=?`,username,(err,result)=>{
@@ -80,6 +94,8 @@ app.post('/count',(req,res)=>{
         res.send(result)
     })
 })
+
+// Post request for showing details of a branch.
 app.post('/details',(req,res)=>{
     const username = String(req.body.username);
     database.query(`SELECT * FROM beetle_nut WHERE username=?`,username,(err,result)=>{
@@ -87,13 +103,22 @@ app.post('/details',(req,res)=>{
         res.send(result)
     })
 })
+
+//Post request for the admin who is shown all alerts.
 app.get('/admin',(req,res)=>{
     database.query(`SELECT * FROM alerts ORDER BY date DESC`,(err,result)=>{
         if(err) throw err;
         res.send(result)
     })
 })
-//INSERT INTO alerts(branch_id,client,client_email,branch_username,date) VALUES((SELECT id FROM beetle_nut WHERE username="teja"),"bebo","bebo@gmail.com",(SELECT username FROM beetle_nut WHERE username="teja"),CURRENT_TIMESTAMP);
-app.listen(3001, () => {
+server.listen(3001, () => {
     console.log('app is running on port 3001')
+})
+
+// The socket.io connection
+io.on('connection',(socket)=>{
+    //console.log('user connected: '+socket.id)
+    socket.on('alert',(data)=>{
+        socket.broadcast.emit('alert',data)
+    })
 })
